b9671a0269d2a40f2459d028e90a81692ef8bb8d
Your Name <you@example.com>
2026-04-08 17:59:57 +0000

    Fix Flask binding for Kubernetes (0.0.0.0)

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def add(a, b):
 
 def main():
     # Bind only to localhost for security (recommended by Sonar)
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
