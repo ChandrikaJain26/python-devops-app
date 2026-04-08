b1186ec0f92852eae5d3c8f27938fa71abe0c012
ChandrikaJain26 <chandrika2683@gmail.com>
2026-04-08 15:39:18 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "UP"}), 200
 
-@app.route("/add/<int:a>/<int:b>")
+@app.route("/add/<int:a>/<int:b>", methods=["GET"])
 def add(a, b):
     return jsonify({"result": a + b}), 200
 
 def main():
-    # Bind on 0.0.0.0 so it works inside Docker also
-    app.run(host="0.0.0.0", port=8080)
+    # Bind only to localhost for security (recommended by Sonar)
+    app.run(host="127.0.0.1", port=8080)
 
 if __name__ == "__main__":
     main()
